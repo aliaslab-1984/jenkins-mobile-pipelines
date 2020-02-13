9cd337c8587cae8d2dfdeab354eb3d372d89773c
icesco <53116975+icesco@users.noreply.github.com>
2020-02-13 15:56:53 +0100

    Removed icon

--- vars/safeCall.groovy
+++ vars/safeCall.groovy
@@ -48,8 +48,7 @@ def call(Closure closure) {
    
    try {
       
-     slackSend color: '#ffff00', 
-        icon_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Apple_logo_dark_grey.svg/98px-Apple_logo_dark_grey.svg.png",
+     slackSend color: '#ffff00',
         message: """
 Thread: ${extracted}
 Platform: ${platform}
