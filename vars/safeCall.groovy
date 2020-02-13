274edeab906d112c8b52c46ce423d1e787620b79
icesco <53116975+icesco@users.noreply.github.com>
2020-02-13 12:04:22 +0100

    Added threads symbols for each execution.

--- vars/safeCall.groovy
+++ vars/safeCall.groovy
@@ -1,7 +1,24 @@
 def call(Closure closure) {
+   
+   def threads = [":large_green_circle:", 
+              ":large_yellow_circle:",
+              ":large_purple_circle:",
+              ":large_brown_circle:",
+              ":large_orange_circle:",
+              ":large_green_square:", 
+              ":large_yellow_square:",
+              ":large_purple_square:",
+              ":large_brown_square:",
+              ":large_orange_square:",
+              ]
+
+   Random random = new Random()
+   def randomIndex = random.nextInt(threads.size() - 1)
+   def extracted = threads[randomIndex]
+   
    try {
      slackSend color: '#ffff00', message: """
-     ${env.JOB_NAME}
+     ${extracted} ${env.JOB_NAME}
      Build: #${env.BUILD_NUMBER}
      Status: *Started* on :computer: ${NODE_NAME}
      See: <${BUILD_URL}|here>
@@ -10,7 +27,7 @@ def call(Closure closure) {
      closure();
 
      slackSend color: 'good', message: """
-     ${env.JOB_NAME}
+     ${extracted} ${env.JOB_NAME}
      Build: #${env.BUILD_NUMBER}
      Status: :tada: *Succeded* :tada: on ${NODE_NAME}
      See: <${BUILD_URL}|here>
@@ -18,7 +35,7 @@ def call(Closure closure) {
    }
    catch (Exception | AssertionError exc) {
      slackSend color: '#ff0000', message: """
-     ${env.JOB_NAME}
+     ${extracted} ${env.JOB_NAME}
      Build: #${env.BUILD_NUMBER}
      Status: :boom: *Failed* on ${NODE_NAME}
      See: <${BUILD_URL}|here>
